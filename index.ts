929315de45d3bd2277025b0f32f43d5947849073
Nicholas Azar <nicholas@nicholasazar.com>
2016-11-20 18:18:06 -0500

    Rewording intro paragraph

--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
-export * from './src/app/ng2-selectize/ng2-selectize.directive.ts';
+import Ng2SelectizeModule from "./src/app/ng2-selectize/ng2-selectize.module";
+export default Ng2SelectizeModule;
