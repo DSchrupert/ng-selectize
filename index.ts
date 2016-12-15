538fbf9687e2cf4fda9e9f9a4b0c6df748af06c2
Nicholas Azar <nicholas@nicholasazar.com>
2016-12-14 20:29:55 -0500

    Fix for #3.

--- index.ts
+++ index.ts
@@ -1,1 +1,2 @@
-export {Ng2SelectizeModule} from "./src/ng2-selectize/ng2-selectize.module";
+import {Ng2SelectizeModule} from "./src/ng2-selectize/ng2-selectize.module";
+export {Ng2SelectizeModule};
