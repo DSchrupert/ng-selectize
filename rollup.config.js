117a4c82d738887b99516cedb43173de618c72fc
Nicholas Azar <nicholas@nicholasazar.com>
2017-02-19 00:18:33 -0500

    removing rollup node resolver

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-
 export default {
 	entry: 'dist/index.js',
 	dest: 'dist/bundles/ng2-selectize.umd.js',
@@ -10,15 +8,5 @@ export default {
 		'@angular/core': 'ng.core',
 		'@angular/common': 'ng.common',
 		'@angular/forms': 'ng.forms',
-	},
-	plugins: [
-		nodeResolve({
-			module: true,
-			jsnext: true,
-			main: true,
-			extensions: ['.js', '.json', '.ts'],
-			skip: ['@angular/core', '@angular/common', '@angular/forms'],
-			preferBuiltins: false
-		})
-	]
+	}
 }
